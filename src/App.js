bd47ff5a24edcc63f6026e9137495456bdfffc4a
vvEii <coopershang125@gmail.com>
2021-04-20 13:58:44 -0700

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Nav from './components/Nav';
-
+import Home from './pages/Home';
 function App() {
   return (
     <Router>
       <div className='bg-gray-darkest min-h-screen'>
+        <Nav />
         <Switch>
-          <Route path='/' exact component={Nav} />
+          <Route path='/' exact component={Home} />
           {/* <Route path='/programs' component={} />
         <Route path='/contact' component={} /> */}
         </Switch>
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div className='text-gray-light text-8xl'>Home</div>;
+};
+
+export default Home;
