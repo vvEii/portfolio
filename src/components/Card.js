924253d50922d1137115a2f995537a5f9121914c
vvEii <coopershang125@gmail.com>
2021-04-22 23:42:01 -0700

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='flex flex-col items-start justify-items-center'>
+      <div className='text-4xl text-purple-400'>Project Title</div>
+      <div className='text-2xl text-purple-400'>01/04/2021</div>
+      <div className='text-lg text-purple-400'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id leo in vitae
+        turpis. Arcu cursus vitae congue mauris. Risus at ultrices mi tempus
+        imperdiet. Scelerisque felis imperdiet proin fermentum leo vel orci.
+        Velit euismod in pellentesque massa placerat duis. Enim nunc faucibus a
+        pellentesque sit amet porttitor eget. Volutpat blandit aliquam etiam
+        erat velit scelerisque in. Volutpat lacus laoreet non curabitur gravida
+        arcu ac. Gravida neque convallis a cras semper auctor neque vitae.
+        Elementum eu facilisis sed odio morbi quis. Posuere urna nec tincidunt
+        praesent semper. Vitae tortor condimentum lacinia quis vel eros donec
+        ac. Sit amet consectetur adipiscing elit ut aliquam purus sit amet.
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Card from '../components/Card';
 
 const Projects = () => {
-  return <div className='text-gray-light text-8xl'>Projects</div>;
+  return (
+    <div className='text-gray-light text-8xl'>
+      <Card />
+    </div>
+  );
 };
 
 export default Projects;
