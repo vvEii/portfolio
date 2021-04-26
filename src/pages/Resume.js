08e6d218bed8b5d968afc4938a0f123686bb6091
vvEii <coopershang125@gmail.com>
2021-04-26 14:25:06 -0700

    Enable resume download

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ResumeFrame from '../components/ResumeFrame';
+import { Link } from 'react-router-dom';
 
 const Resume = () => {
   return (
     <div className='px-8'>
       <ResumeFrame />
+      <Link to='/Resume.pdf' target='_blank' download>
+        Download
+      </Link>
     </div>
   );
 };
